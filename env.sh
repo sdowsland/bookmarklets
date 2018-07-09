19afcd9f39bdcea3803a8f9f5f4e8e6c39759907
Stephen Dowsland <s.dowsland@gmail.com>
2018-07-09 19:01:02 +0100

    Update dev build process Add package.json to allow npm install in Dockerfile Build Docker image if not exist

--- env.sh
+++ env.sh
@@ -1,1 +1,11 @@
-docker run --rm -it -v ${PWD}:/usr/local/app -w /usr/local/app node:8 bash
+
+imageName=sdowsland/bookmarklet-dev
+imageExist=$(docker images | grep $imageName | awk '{print $1}')
+
+if [ "$imageExist" != "$imageName" ]
+then
+    docker build -f docker/dev/Dockerfile -t $imageName .
+fi
+
+
+docker run --rm -it -v ${PWD}:/usr/local/app -w /usr/local/app $imageName bash
